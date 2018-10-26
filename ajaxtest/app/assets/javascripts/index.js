bb7cc42404ddef535aae23c2758f86253c407aee
Kirby <jkirbycampbell@gmail.com>
2018-10-26 15:15:54 -0700

    Zoning in

--- ajaxtest/app/assets/javascripts/index.js
+++ ajaxtest/app/assets/javascripts/index.js
@@ -1,44 +1,21 @@
-function showRepositories() {
-  var repos = JSON.parse(this.responseText);
-  console.log(repos);
-  const repoList = `<ul>${repos
-    .map(
-      r =>
-        '<li>' +
-        r.name +
-        ' - <a href="#" data-repo="' +
-        r.name +
-        '" onclick="getCommits(this)">Get Commits</a></li>'
-    )
-    .join('')}</ul>`;
-  document.getElementById('repositories').innerHTML = repoList;
-}
+$(function(){
+  $("form#new_post").on("submit", function(e){
+    e.preventDefault()
+    var $form = $(this);
+    var action =  $form.attr("action")
+    var params = $form.serialize()
 
-function getRepositories() {
-  const req = new XMLHttpRequest();
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
-  req.send();
-}
-
-function getCommits(el) {
-  const name = el.dataset.repo;
-  const req = new XMLHttpRequest();
-  req.addEventListener('load', showCommits);
-  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
-  req.send();
-}
-
-function showCommits() {
-  const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul>${commits
-    .map(
-      commit =>
-        '<li><strong>' +
-        commit.author.login +
-        '</strong> - ' +
-        commit.commit.message +
-        '</li>'
-    )
-    .join('')}</ul>`;
-  document.getElementById('commits').innerHTML = commitsList;
-}
+    $.ajax({
+      url: action,
+      data: params,
+      dataType: "json",
+      method: "POST "
+    })
+    .success(function(json){
+      console.log(json)
+    })
+    .error(function(response){
+      console.log("You broke it?", response)
+    })
+  })
+})
--- ajaxtest/app/controllers/posts_controller.rb
+++ ajaxtest/app/controllers/posts_controller.rb
@@ -4,16 +4,26 @@ class PostsController < ApplicationController
     @post = Post.new
     @posts = Post.all
 
-    respond_to do |format|
-      format.html {render 'posts/index.html', :layout => false}
-      format.js {render 'posts/index.js', :layout => false}
-    end
+#    respond_to do |format|
+#      format.html {render 'posts/index.html', :layout => false}
+#      format.js {render 'posts/index.js', :layout => false}
+#    end
   end
 
   def create
     @post = Post.new(posts_params)
     if @post.save
+#      respond_to do |f|
+#        f.html {redirect_to post_path(@post)}
+#        f.json {render :json => @post}
+#      end
+    else
+      render "posts/show"
     end
+  end
+
+  def show
+      @post = Post.find_by_id(params[:id])
   end
 
   def new
