56d10cd2fbbc68011e8cd56d6a51554153c29672
Kirby <jkirbycampbell@gmail.com>
2018-10-25 20:16:46 -0700

    Added a test form

--- ajaxtest/app/controllers/posts_controller.rb
+++ ajaxtest/app/controllers/posts_controller.rb
@@ -1,6 +1,21 @@
 class PostsController < ApplicationController
 
   def index
-    
+    @post = Post.new
+    @posts = Post.all
+  end
+
+  def create
+    @post = Post.build(posts_params)
+  end
+
+  def new
+    render :index
+  end
+
+  private
+
+  def posts_params
+    params.require(:post).permit(:content)
   end
 end
